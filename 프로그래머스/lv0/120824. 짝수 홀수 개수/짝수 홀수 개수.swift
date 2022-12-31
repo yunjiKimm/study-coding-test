458fae87775419e6b3f4004290170a4c838b4c16
김윤지 <yunji.kmm@gmail.com>
2022-12-31 17:15:20 +0900

    Update 짝수 홀수 개수.swift

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.swift
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.swift
@@ -13,3 +13,13 @@ func solution(_ num_list:[Int]) -> [Int] {
     
     return answer
 }
+
+// =====================
+// short coding
+// =====================
+
+import Foundation
+
+func solution(_ num_list:[Int]) -> [Int] {
+    return [num_list.filter{$0 % 2 == 0}.count, num_list.filter{$0 % 2 == 1}.count]
+}
