e7790374b2924f2522ecbe64689fc91cf740e092
김윤지 <yunji.kmm@gmail.com>
2023-01-10 15:14:44 +0900

    [level 0] Title: 모스부호 (1), Time: 0.18 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+func solution(_ letter:String) -> String {
+    let morse = [
+        ".-": "a", "-...": "b", "-.-.": "c", "-..": "d", ".": "e", "..-.": "f",
+        "--.": "g", "....": "h", "..": "i", ".---": "j", "-.-": "k", ".-..": "l",
+        "--": "m", "-.": "n", "---": "o", ".--.": "p", "--.-": "q", ".-.": "r",
+        "...": "s", "-": "t", "..-": "u", "...-": "v", ".--": "w", "-..-": "x",
+        "-.--": "y", "--..": "z"
+    ]
+    return letter.components(separatedBy: " ").map{ morse[$0] ?? "" }.joined()
+}
