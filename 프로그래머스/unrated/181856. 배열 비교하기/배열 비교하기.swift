4dcb3c42633e91e2d05eba140295feadf6626bb4
Yunji Kim <yunji.kmm@gmail.com>
2024-01-06 16:16:55 +0900

    [level unrated] Title: 배열 비교하기, Time: 0.02 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func solution(_ arr1:[Int], _ arr2:[Int]) -> Int {
+    if arr1.count > arr2.count {
+        return 1
+    } else if arr1.count < arr2.count {
+        return -1
+    } else {
+        if arr1.reduce(0, +) > arr2.reduce(0, +) {
+            return 1
+        } else if arr1.reduce(0, +) < arr2.reduce(0, +) {
+            return -1
+        } else {
+            return 0
+        }
+    }
+}
