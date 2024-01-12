760d7fc2168d4b60a5fa06d0c4b00a069b22d67a
Yunji Kim <yunji.kmm@gmail.com>
2024-01-12 16:52:03 +0900

    [level Lv.0] Title: 간단한 식 계산하기, Time: 0.10 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181865. 간단한 식 계산하기/README.md
+++ 프로그래머스/Lv.0/181865. 간단한 식 계산하기/README.md
@@ -1,10 +1,10 @@
 # [level Lv.0] 간단한 식 계산하기 - 181865 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181865) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181865?language=swift) 
 
 ### 성능 요약
 
-메모리: 16.5 MB, 시간: 0.11 ms
+메모리: 16.3 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 5일 16:49:14
+2024년 1월 5일 16:52:1
 
 ### 문제 설명
 
--- 프로그래머스/Lv.0/181865. 간단한 식 계산하기/간단한 식 계산하기.swift
+++ 프로그래머스/Lv.0/181865. 간단한 식 계산하기/간단한 식 계산하기.swift
@@ -4,22 +4,20 @@ func solution(_ binomial:String) -> Int {
     let binomial_list = binomial.components(separatedBy: " ")
     var result: Int = 0
     
-    for _ in 0 ..< binomial_list.count {
-        guard let first_num = Int(binomial_list[0]),
-              let second_num = Int(binomial_list[2]) else {
-                  return 0
-              }
-        
-        switch binomial_list[1] {
-        case "+":
-            result = first_num + second_num
-        case "-":
-            result = first_num - second_num
-        case "*":
-            result = first_num * second_num
-        default:
-            break
-        }
+    guard let first_num = Int(binomial_list[0]),
+          let second_num = Int(binomial_list[2]) else {
+              return 0
+          }
+
+    switch binomial_list[1] {
+    case "+":
+        result = first_num + second_num
+    case "-":
+        result = first_num - second_num
+    case "*":
+        result = first_num * second_num
+    default:
+        break
     }
     
     return result
