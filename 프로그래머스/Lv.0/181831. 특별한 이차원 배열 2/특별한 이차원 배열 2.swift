954eea9e2273ad0a8e73d38d95f9b30905f0b1dd
Yunji Kim <yunji.kmm@gmail.com>
2024-01-13 17:29:20 +0900

    [level Lv.0] Title: 특별한 이차원 배열 2, Time: 0.01 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ arr:[[Int]]) -> Int {
+    for i in 0 ..< arr.count {
+        for j in 0 ..< arr.count {
+            if arr[i][j] != arr[j][i] {
+                return 0
+            }
+        }
+    }
+
+    return 1
+}
